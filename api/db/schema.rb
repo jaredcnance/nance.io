a38b0659782ff795cef26400ed7d6a198047e368
jaredcnance <jaredcnance@gmail.com>
2016-01-26 20:40:15 -0600

    protect api by requiring a key for write methods

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125144914) do
+ActiveRecord::Schema.define(version: 20160127020532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "website_visits", force: :cascade do |t|
     t.integer  "website_id"
