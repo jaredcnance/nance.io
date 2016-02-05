1f25a54ecccc8784be9ef8630817f7786a9ef9d2
jaredcnance <jaredcnance@gmail.com>
2016-02-04 20:51:19 -0600

    add launch route

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'launch#index'
   jsonapi_resources :websites
   jsonapi_resources :website_visits
 end
---
+++ api/spec/controllers/launch_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe LaunchController, type: :controller do
+
+end
