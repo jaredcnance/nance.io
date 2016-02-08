cccada1023dd41830ccca9590305c00afd36cdc6
jaredcnance <jaredcnance@gmail.com>
2016-02-08 08:32:30 -0600

    pull current index from reds

--- api/app/controllers/launch_controller.rb
+++ api/app/controllers/launch_controller.rb
@@ -1,6 +1,6 @@
 class LaunchController < ApplicationController
   def index
-    html = $redis.get('nance:index:default') #:current-content')
+    html = $redis.get('nance:index:current') #:current-content')
     render :text => html
   end
 end
