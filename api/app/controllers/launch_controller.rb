07ac3ad36aa907c525749c3827431fcf64448f61
jaredcnance <jaredcnance@gmail.com>
2016-02-08 08:34:55 -0600

    content

--- api/app/controllers/launch_controller.rb
+++ api/app/controllers/launch_controller.rb
@@ -1,6 +1,6 @@
 class LaunchController < ApplicationController
   def index
-    html = $redis.get('nance:index:current') #:current-content')
+    html = $redis.get('nance:index:current-content') #:current-content')
     render :text => html
   end
 end
